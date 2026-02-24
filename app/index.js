ca1d2ac4b3b59a5343bba7a13d63b7a8d016b73a
[200~@Ruthwikreddyladdu <ruthwikreddyladdu@gmail.com>
2026-02-24 19:13:58 +0530

    Testing Day 6 auto deploy

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevOps CI/CD Project Running 🚀");
+  res.send("CloudForge Day 6 Auto Deploy Successful 🚀");
 });
 
 app.get("/health", (req, res) => {
