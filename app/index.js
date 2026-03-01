80b8228dd383bb070330180cf62047d5c3b1c0df
[200~@Ruthwikreddyladdu <ruthwikreddyladdu@gmail.com>
2026-03-01 11:39:02 +0530

    Add CPU load for auto scaling test

--- app/index.js
+++ app/index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
 
+// Root route (adds CPU load to trigger auto scaling)
 app.get("/", (req, res) => {
-  res.send("CloudForge Day 6 Auto Deploy Successful 🚀");
+  const start = Date.now();
+
+  // Burn CPU for ~300ms
+  while (Date.now() - start < 300) {}
+
+  res.send("CloudForge Auto Scaling Test Successful 🚀");
 });
 
+// Health check route (used by ALB target group)
 app.get("/health", (req, res) => {
   res.send("OK");
 });
 
+// Start server
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
